c57496508ce9471d751609c2e547e9f9ee4c7892
Polar4ik <atossnikitoss@gmail.com>
2024-11-17 00:27:13 +0400

    remove coments from "build.sh"

--- build.sh
+++ build.sh
@@ -3,10 +3,8 @@
 SOURCE="main.c"
 OUTPUT="main.exe"
 
-# Компиляция
 clang "$SOURCE" -o "$OUTPUT" -D_CRT_SECURE_NO_WARNINGS
 
-# Проверка кода возврата компиляции
 if [ $? -eq 0 ]; then
     echo "Compilation successful. Executable created: $OUTPUT"
     echo
