5c2b915d326d765e0c51bee1fd8b61210f82f251
Polar4ik <atossnikitoss@gmail.com>
2024-11-17 01:36:31 +0400

    Add relization for SUb and add MUL & DIV opcode

--- main.c
+++ main.c
@@ -15,11 +15,13 @@ typedef struct {
 } Program;
 
 typedef enum Opcode {
-    PUSH,
-    PRINT,
+    PUSH, // PUSH <reg> <value>
+    PRINT, // PRINT <reg>
 
-    ADD,
-    SUB,
+    ADD, // ADD <reg1> <reg2>
+    SUB, // SUB <reg1> <reg2>
+    MUL, // MUL <reg1> <reg2>
+    DIV, // DIV <reg1> <reg2>
 } Opcode;
 
 Program *init_program() {
@@ -45,7 +47,9 @@ int get_reg_num(char *str) {
 }
 
 int opcode_arg_num(char *opcode) {
-    if (strcmp(opcode, "PUSH") == 0 || strcmp(opcode, "ADD") == 0) {
+    if (strcmp(opcode, "PUSH") == 0\
+    || strcmp(opcode, "ADD") == 0 || strcmp(opcode, "SUB") == 0\
+    || strcmp(opcode, "MUL") == 0 || strcmp(opcode, "DIV") == 0) {
         return 2;
     }
     else if (strcmp(opcode, "PRINT") == 0) {
@@ -56,22 +60,19 @@ int opcode_arg_num(char *opcode) {
 }
 
 Opcode get_opcode(char *str) {
-    if (strcmp(str, "PUSH") == 0) {
-        return PUSH;
-    }
-    else if (strcmp(str, "ADD") == 0) {
-        return ADD;
-    }
-    else if (strcmp(str, "PRINT") == 0) {
-        return PRINT;
-    }
+    if (strcmp(str, "PUSH") == 0) return PUSH;
+    else if (strcmp(str, "ADD") == 0) return ADD;
+    else if (strcmp(str, "SUB") == 0) return SUB;
+    else if (strcmp(str, "MUL") == 0) return MUL;
+    else if (strcmp(str, "DIV") == 0) return DIV;
+    else if (strcmp(str, "PRINT") == 0) return PRINT;
     
     printf("Err Get Opcode: Unkown Opcode");
     exit(1);
 }
 
 void exec_instruct(Program *program, Opcode instruct, char **args) {
-    int reg_num = 0;
+    int reg_num, reg_1, reg_2 = 0;
     switch (instruct) {
         case PUSH:
             reg_num = get_reg_num(args[0]);
@@ -79,16 +80,35 @@ void exec_instruct(Program *program, Opcode instruct, char **args) {
             program->registers->list[reg_num] = value;
             break;
         case ADD:
-            int reg_1 = get_reg_num(args[0]);
-            int reg_2 = get_reg_num(args[1]);
+            reg_1 = get_reg_num(args[0]);
+            reg_2 = get_reg_num(args[1]);
             program->registers->list[reg_1] += program->registers->list[reg_2];
+            break;
+        case SUB:
+            reg_1 = get_reg_num(args[0]);
+            reg_2 = get_reg_num(args[1]);
+            program->registers->list[reg_1] -= program->registers->list[reg_2];
+            break;
+        case MUL:
+            reg_1 = get_reg_num(args[0]);
+            reg_2 = get_reg_num(args[1]);
+            program->registers->list[reg_1] *= program->registers->list[reg_2];
+            break;
+        case DIV:
+            reg_1 = get_reg_num(args[0]);
+            reg_2 = get_reg_num(args[1]);
+            if (program->registers->list[reg_1] == 0 || program->registers->list[reg_2] == 0) {
+                printf("Err: Divide zero\n");
+                return;
+            }
+            program->registers->list[reg_1] /= program->registers->list[reg_2];
             break;
         case PRINT:
             reg_num = get_reg_num(args[0]);
             printf("R%d: %d\n", reg_num, program->registers->list[reg_num]);
             break;
         default:
-            printf("Err Exec Instruct: Unkown Opcode");
+            printf("Err Exec Instruct: Unkown Opcode\n");
             exit(1);
     }
 }
@@ -124,7 +144,7 @@ void exec_program(Program *program, const char *program_text) {
 }
 
 int main(void) {
-    printf("\nINTERPRITER START\n");
+    printf("\nINTERPRETER START\n");
     printf("-----------------\n\n");
     Program *program = init_program();
     
@@ -136,6 +156,14 @@ int main(void) {
         PRINT R1\
         ADD R0 R1\
         PRINT R0\
+        PUSH R1 2\
+        MUL R0 R1\
+        PRINT R0\
+        PUSH R1 30\
+        PRINT R0\
+        PRINT R1\
+        DIV R1 R0\
+        PRINT R1\
     ";
 
     exec_program(program, text);
